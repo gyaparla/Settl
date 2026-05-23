74d3d22e5e1a461564dc0c1b9b0edbb4051cdeb8
Gangadhara Reddy Yaparla <yaparlagangadhara@gmail.com>
2026-05-23 14:39:57 +0530

    first commit in the dev

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+      <h1 className="text-3xl font-bold underline">
+        Hello world! all features coming soon...
+      </h1>{" "}
     </>
   );
 }
